446de2e8caf2e8d806e7aee6beab80380eb26407
profbiscuit <matthew.buras@yahoo.com>
2017-09-12 20:39:02 -0700

    read in bgg data as csv using pandas package. also, i drank your milkshake <(^_^)>

--- hi.py
+++ hi.py
@@ -1,1 +1,12 @@
-print "hi"
+print ("hi")
+
+print ("i drink your milkshake")
+
+import pandas as pd
+import numpy as np
+import matplotlib as mp
+
+bgg = pd.read_csv("C:/Users/Biscuit/Dropbox/bgg_db_2017_04.csv", encoding = "latin-1")
+
+print(bgg)
+
